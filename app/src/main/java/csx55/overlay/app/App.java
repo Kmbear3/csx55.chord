b1bc4633b6da5df0fba20c061d58886dfeb743e3
Kass <kmbarram@gmail.com>
2024-01-19 13:14:32 -0700

    remove unused imports

--- app/src/main/java/csx55/overlay/app/App.java
+++ app/src/main/java/csx55/overlay/app/App.java
@@ -3,14 +3,6 @@
  */
 package csx55.overlay.app;
 
-import csx55.overlay.list.LinkedList;
-
-import static csx55.overlay.utilities.StringUtils.join;
-import static csx55.overlay.utilities.StringUtils.split;
-import static csx55.overlay.app.MessageUtils.getMessage;
-
-import org.apache.commons.text.WordUtils;
-
 public class App {
     public static void main(String[] args) {
         // LinkedList tokens;
