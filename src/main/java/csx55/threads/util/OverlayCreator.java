fda686e954bcfabc37efb6f3a72b01a626ed230f
Kass <kmbarram@gmail.com>
2024-02-19 18:39:52 -0700

    remove cr connection code

--- src/main/java/csx55/threads/util/OverlayCreator.java
+++ src/main/java/csx55/threads/util/OverlayCreator.java
@@ -12,13 +12,13 @@ import csx55.threads.wireformats.MessagingNodesList;
 
 public class OverlayCreator {
     // Needs registered nodes
-    int numberOfConnections;
+    int numberOfThreads;
     VertexList registeredNodes;
     final ArrayList<String> names;
     int[][] linkWeights; 
        
-    public OverlayCreator(Registry registry, int numberOfConnections){
-        this.numberOfConnections = numberOfConnections;
+    public OverlayCreator(Registry registry, int numberOfThreads){
+        this.numberOfThreads = numberOfThreads;
         this.registeredNodes = registry.getRegistry();
         this.names = registeredNodes.getVertexNames();
         constructOverlay();
@@ -27,7 +27,7 @@ public class OverlayCreator {
     synchronized public void constructOverlay(){
         try {
             
-            int[][] crConnections = assignConnections();
+            int[][] crConnections = constructRing();
             this.linkWeights = crConnections;
 
             assignNeighbors(crConnections);
@@ -35,7 +35,6 @@ public class OverlayCreator {
             // Below Just sends the messages
 
             for(Vertex vertex : registeredNodes.getValues()){
-                // vertex.printVertex();
                 
                 MessagingNodesList nodesList = new MessagingNodesList(vertex.getVertexConnections());
                 vertex.sendMessage(nodesList.getBytes());
@@ -75,131 +74,6 @@ public class OverlayCreator {
             connections[(i + 1) % connections.length][i] = linkWeight;  
         }
         return connections;
-    }
-
-    public int[][] assignConnections(){
-        Random rand = new Random();
-        int[][] connections = constructRing();
-        // int CR = numberOfConnections;
-        int CR = connections.length -2;
-
-
-        while(CR > 1){
-            for(int j = 0; j < connections.length; j++){
-                if(!isFullyConnected(j, connections, numberOfConnections)){
-                    int weight =  rand.nextInt(10) + 1;
-                    // if((j + CR) % connections.length == j) {
-                    //     continue; // SUS Super sus
-                    // }
-                    // else{
-                        connections[j][(j + CR) % connections.length] = weight;
-                        connections[(j + CR) % connections.length][j] = weight;
-                    // }
-                }
-            }
-
-            if(!isBalanced(connections)){
-                rewindConnections(CR, connections);
-            }
-
-            CR--;
-        }
-
-        return connections;
-    }
-
-    synchronized public void rewindConnections(int CR, int[][] connections){
-        for(int j = 0; j < connections.length; j++){
-            connections[j][(j + CR) % connections.length] = 0;
-            connections[(j + CR) % connections.length][j] = 0;
-        }
-    }
-
-    synchronized public void printConnections(int[][] matrix){
-
-        for(String name : names){
-            System.out.print(" | " + name + " | ");
-        }
-        System.out.println();
-
-        System.out.print(" | - | ");
-        for(int i = 0; i < names.size(); i ++){
-            System.out.print(" | " + i + " | ");
-        }
-        System.out.println();
-
-        for(int i = 0; i < matrix.length; i++){
-            System.out.print(" | " + i + " | ");
-            for(int j = 0; j < matrix.length; j ++){
-                System.out.print(" | " + matrix[i][j] + " | ");
-            }
-            System.out.println();
-        }
-
-    }
-
-    synchronized public boolean isFullyConnected(int node, int[][] connections, int CR){
-        int[] nodeConnections = connections[node];
-        int nodeNumberOfConnections = 0;
-
-        for(int i = 0; i < nodeConnections.length; i ++){
-            if(nodeConnections[i] != 0){
-                nodeNumberOfConnections++;
-            }
-        }
-
-
-        return nodeNumberOfConnections == CR;
-    }
-
-    synchronized public boolean isBalanced(int[][] matrix){
-
-        int[] nodeConnections = matrix[0];
-        int nodeNumberOfConnections = 0;
-
-        for(int i = 0; i < nodeConnections.length; i ++){
-            if(nodeConnections[i] != 0){
-                nodeNumberOfConnections++;
-            }
-        }
-
-        for(int i = 0; i < matrix.length; i++){
-            int rowCount = 0;
-            for(int j = 0; j < matrix.length; j ++){
-                if(matrix[i][j] != 0){
-                    rowCount++;
-                }
-            }
-            if(rowCount != nodeNumberOfConnections){
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public int[][] getConnections(){
-        return linkWeights;
-    }
-
-    synchronized public ArrayList<String> createLinkInfo(){
-        ArrayList<String> linkInfos = new ArrayList<>();
-
-        for(int i = 0;  i < linkWeights.length; i ++ ){
-            for(int j = 0; j < linkWeights.length; j ++){
-                linkInfos.add(names.get(i) + " " + names.get(j) + " " + linkWeights[i][j]);
-            }
-        }
-
-        return linkInfos;
-    }
-
-    synchronized public void listWeights() {
-        for(String connection : createLinkInfo()){
-            if(Integer.parseInt(connection.split(" ")[2]) != 0){
-                System.out.println(connection);
-            }
-        }
     }
 }
 
