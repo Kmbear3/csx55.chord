66eb3fc8d13abd3a7668a187645776833388ad3b
Kass <kmbarram@gmail.com>
2024-02-21 14:58:02 -0700

    touches

--- src/main/java/csx55/threads/wireformats/Tasks.java
+++ src/main/java/csx55/threads/wireformats/Tasks.java
@@ -7,11 +7,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.Socket;
 import java.util.ArrayList;
 
 import csx55.threads.hashing.Task;
-import csx55.threads.util.Vertex;
 
 public class Tasks implements Protocol, Event{
     ArrayList<Task> taskList;
